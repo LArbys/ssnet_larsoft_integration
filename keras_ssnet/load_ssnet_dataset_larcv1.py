e2cfe32b6332ea5c2ebc22aba521b390a09e9c99
twongjirad <taritree.wongjirad@gmail.com>
2018-08-28 18:20:25 -0400

    added simple larcv1 loader

--- keras_ssnet/load_ssnet_dataset_larcv1.py
+++ keras_ssnet/load_ssnet_dataset_larcv1.py
@@ -1,7 +1,9 @@
-import os,sys
-from ROOT import larcv
+import os,sys,time
+import ROOT as rt
+from larcv import larcv
+from larcvdataset.larcv1dataset import LArCV1Dataset
 
-def load_ssnet_dataset_larcv1( trainingfiles, testingfiles ):
+def load_ssnet_dataset_larcv1( trainingfiles, testingfiles, randomize=True ):
     # define configurations
     if type(trainingfiles) is not list or type(testingfiles) is not list:
         raise ValueError("trainingfiles or testingfiles arguments must be a list of file paths")
@@ -12,104 +14,48 @@ def load_ssnet_dataset_larcv1( trainingfiles, testingfiles ):
         if not os.path.exists(s):
             raise ValueError("File %s does not exist"%(s))
     
-    traincfg = """ThreadDatumFillerTrain: {
-  Verbosity:    2
-  EnableFilter: false
-  RandomAccess: true
-  UseThread:    false
-  InputFiles:   {}
-  ProcessType:  ["SegFiller"]
-  ProcessName:  ["SegFiller"]
 
-  IOManager: {
-    Verbosity: 2
-    IOMode: 0
-    ReadOnlyTypes: [0,0,0]
-    ReadOnlyNames: ["wire","segment","ts_keyspweight"]
-  }
-    
-  ProcessList: {
-    SegFiller: {
-      # DatumFillerBase configuration
-      Verbosity: 2
-      ImageProducer:     "wire"
-      LabelProducer:     "segment"
-      WeightProducer:    "ts_keyspweight"
-      # SegFiller configuration
-      Channels: [2]
-      SegChannel: 2
-      EnableMirror: false
-      EnableCrop: false
-      ClassTypeList: [0,1,2]
-      ClassTypeDef: [0,0,0,2,2,2,1,1,1,1]
-    }
-  }
-}
-"""
-    validcfg = """ThreadDatumFillerValid: {
-  Verbosity:    2
-  EnableFilter: false
-  RandomAccess: true
-  UseThread:    false
-  InputFiles:   {}
-  ProcessType:  ["SegFiller"]
-  ProcessName:  ["SegFiller"]
-
-  IOManager: {
-    Verbosity: 2
-    IOMode: 0
-    ReadOnlyTypes: [0,0,0]
-    ReadOnlyNames: ["wire","segment","ts_keyspweight"]
-  }
-    
-  ProcessList: {
-    SegFiller: {
-      # DatumFillerBase configuration
-      Verbosity: 2
-      ImageProducer:     "wire"
-      LabelProducer:     "segment"
-      WeightProducer:    "ts_keyspweight"
-      # SegFiller configuration
-      Channels: [2]
-      SegChannel: 2
-      EnableMirror: false
-      EnableCrop: false
-      ClassTypeList: [0,1,2]
-      ClassTypeDef: [0,0,0,2,2,2,1,1,1,1]
-    }
-  }
-}
-"""
-    traincfg = traincfg.format( trainingfiles )
-    validcfg = validcfg.format( testingfiles )
-    with open("ssnet_segfiller_train.cfg",'w') as ftrain:
-        print >> ftrain,traincfg
-    with open("ssnet_segfiller_valid.cfg",'w') as fvalid:
-        print >> fvalid,validcfg
-    
-    iotrain = LArCV1Dataset("ThreadDatumFillerTrain","ssnet_segfiller_train.cfg" )
-    iovalid = LArCV1Dataset("ThreadDatumFillerValid","ssnet_segfiller_valid.cfg" )
-    iotrain.init()
-    iovalid.init()
+    productlists =  [(larcv.kProductImage2D,"wire"),    # adc values
+                     (larcv.kProductImage2D,"segment"), # labels
+                     (larcv.kProductImage2D,"ts_keyspweight")]
+    iotrain = LArCV1Dataset(trainingfiles,productlists,randomize=randomize)
+    iovalid = LArCV1Dataset(testingfiles,productlists,randomize=False)
 
     return iotrain,iovalid
 
 
 if __name__=="__main__":
 
-    trainingfiles = ["val.root"]
-    testingfiles  = ["val.root"]
+    trainingfiles = ["../trainingdata/val.root"]
+    testingfiles  = ["../trainingdata/val.root"]
     iotrain, iovalid = load_ssnet_dataset_larcv1( trainingfiles, testingfiles )
+
     train_nentries = iotrain.io.get_n_entries()
     valid_nentries = iovalid.io.get_n_entries()
     print "Training number of events: ",train_nentries
     print "Validation number of events: ",valid_nentries
 
-    data = iotrain.getbatch(1)
-    print data
-    print __dict__(data)
+    tloop = time.time()
+    batchsize = 5
+    nbatches = 10
+    for i in range(0,nbatches):
+        data = iotrain.getbatch( batchsize )
+        for d,array in data.items():
+            print d,array.shape
+        print data["_rse_"]
+    ttrain = time.time()-tloop
 
-    iotrain.stop()
-    iovalid.stop()
+
+    tloop = time.time()    
+    for i in range(0,nbatches):
+        data = iovalid.getbatch( batchsize )
+        for d,array in data.items():
+            print d,array.shape
+        print data["_rse_"]
+    tloop = time.time()-tloop
+
+    print "For batch size of %d"%(batchsize)
+    print "Time for training set: %.2f secs (%.2f secs/batch)"%(ttrain,ttrain/nbatches)    
+    print "Time for validation set: %.2f secs (%.2f secs/batch)"%(tloop,tloop/nbatches)    
     
     
